4ec254a6414240fe0418da120c46bee6e3011aa4
Yash Chouhan <yashchauhanyash@gmail.com>
2025-08-03 09:38:14 +0530

    My first commit in java rep 2025

---
+++ Java_Practice_2025/src/stringConcepts/HelloWord.java
@@ -0,0 +1,15 @@
+package stringConcepts;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Hello";
+		String str2="World";
+		String concat=str+str2;
+		System.out.println( "String concatination: "+concat);
+				
+		
+	}
+
+}
